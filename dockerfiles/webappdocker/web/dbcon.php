de2c68c9bdf3f42a19b835429dd7046866df689c
You Fu <74343657+youfuyfyf@users.noreply.github.com>
2022-10-17 14:48:02 +0800

    Update dbcon.php

--- dockerfiles/webappdocker/web/dbcon.php
+++ dockerfiles/webappdocker/web/dbcon.php
@@ -1,9 +1,14 @@
 <?php
-$con = mysqli_connect("172.16.0.2","root","abc123","3204_db");
 
-// Check connection
-if (mysqli_connect_errno())
-  {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  }
+$host = "172.16.0.2";    
+$port = 3306;
+$user = "root";
+$password = "abc123";
+$dbname = "3204_db";
+
+$con = new mysqli($host, $user, $password, $dbname, $port);
+
+if ($con->connect_error) {
+    die("Connection failed: " . $con->connect_error);
+}
 ?>

66395b643aa95063b02237d0502173967019469b
You Fu <74343657+youfuyfyf@users.noreply.github.com>
2022-10-17 22:51:13 +0800

    Update index.php

--- dockerfiles/webappdocker/web/index.php
+++ dockerfiles/webappdocker/web/index.php
@@ -25,16 +25,16 @@
   <?php
 	if (isset($_POST['login']))
 		{
-			$username = mysqli_real_escape_string($con, $_POST['user']);
-			$password = mysqli_real_escape_string($con, $_POST['pass']);
+			$username = $_POST['user'];
+			$password = $_POST['pass'];
 			
-			$query 		= mysqli_query($con, "SELECT * FROM users WHERE  password='$password' and username='$username'");
+			$query 		= mysqli_query($con, "SELECT * FROM Accounts WHERE  Password='$password' and Username='$username'");
 			$row		= mysqli_fetch_array($query);
 			$num_row 	= mysqli_num_rows($query);
 			
 			if ($num_row > 0) 
 				{			
-					$_SESSION['user_id']=$row['user_id'];
+					$_SESSION['user_id']=$row['User_id'];
 					header('location:home.php');
 					
 				}

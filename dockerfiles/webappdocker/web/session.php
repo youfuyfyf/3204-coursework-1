bf4d403c8730d5ead094edb7ad4c21cc2163b1ec
You Fu <74343657+youfuyfyf@users.noreply.github.com>
2022-10-19 01:54:49 +0800

    v5

--- dockerfiles/webappdocker/web/session.php
+++ dockerfiles/webappdocker/web/session.php
@@ -2,7 +2,7 @@
 //Start session
 session_start();
 //Check whether the session variable SESS_MEMBER_ID is present or not
-if (!isset($_SESSION['user_id'])) {
+if (!isset($_SESSION['user_id']) || (trim($_SESSION['user_id']) == '')) {
     header("location: index.php");
     exit();
 }
--- dockerfiles/webappdocker/web/upload.php
+++ dockerfiles/webappdocker/web/upload.php
@@ -1,22 +1,17 @@
-
-<!DOCTYPE html>
-<html>
-<?php session_start(); ?>
-<?php include('dbcon.php'); ?>
-<body>
-</body>
-</html>
+<form enctype="multipart/form-data" action="upload.php" method="POST">
+<input type="hidden" name="MAX_FILE_SIZE" value="100000" />
+Choose a file to upload: <input name="uploadedfile" type="file" /><br />
+<input type="submit" value="Upload File" />
+</form>
 
 <?php
-
-$target_path = "uploads/";
+$target_path="uploads/";
 //Here we set the target path that will save the file in to.
-$target_path = $target_path . basename($_FILES['fileToUpload']['name']);
+$target_path = $target_path.basename($_FILES['uploadedfile']['name']);
 // here wll move the desired file from the tmp directory to the target path
-if (move_uploaded_file($_FILES['fileToUpload']['tmp_name'], $target_path)) {
-    echo "the file " . basename($_FILES['fileToUpload']['name']) . " has been uploaded! ";
-} else {
-     echo "there was an error uploading the file ,please try again!";
+if(move_uploaded_file($_FILES['uploadedfile']['tmp_name'],$target_path)){
+echo "the file " . basename($_FILES['uploadedfile']['name']) . " has been uploaded! ";
+}else {
+echo "there was an error uploading the file ,please try again!";
 }
 ?>
-

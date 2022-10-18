a7084b4d28db84b6c0f336a9595e8e0c64f13c00
gteckseng <51436245+gteckseng@users.noreply.github.com>
2022-10-18 22:41:38 +0800

    Update upload.php

--- dockerfiles/webappdocker/web/upload.php
+++ dockerfiles/webappdocker/web/upload.php
@@ -18,10 +18,10 @@
 
 $target_path = "uploads/";
 //Here we set the target path that will save the file in to.
-$target_path = $target_path . basename($_FILES['uploadedfile']['name']);
+$target_path = $target_path . basename($_FILES['fileToUpload']['name']);
 // here wll move the desired file from the tmp directory to the target path
-if (move_uploaded_file($_FILES['uploadedfile']['tmp_name'], $target_path)) {
-    echo "the file " . basename($_FILES['uploadedfile']['name']) . " has been uploaded! ";
+if (move_uploaded_file($_FILES['fileToUpload']['tmp_name'], $target_path)) {
+    echo "the file " . basename($_FILES['fileToUpload']['name']) . " has been uploaded! ";
 } else {
      echo "there was an error uploading the file ,please try again!";
 }

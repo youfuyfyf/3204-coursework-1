38a407a6dfb9bc59c7ef491dd942a68092ddc085
You Fu <74343657+youfuyfyf@users.noreply.github.com>
2022-10-21 00:57:05 +0800

    Removed starting service commands

--- scripts/servers_script.sh
+++ scripts/servers_script.sh
@@ -6,12 +6,7 @@ chmod -R 755 /vagrantshared/*
 
 ### Docker Compose ###
 cd /vagrantshared/3204-coursework-1/dockerfiles/
-
 docker compose up -d
-
-docker container exec -d boss service ssh start
-docker container exec -d web service php8.1-fpm start
-docker container exec -d web service nginx start
 
 ### Check Container Status ###
 docker ps
